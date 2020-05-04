dc896446fa7760949c1a836d7b73f8ac8d24b244
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-04 16:18:08 -0700

    Updates

---
+++ precache-manifest.51e454a7f4b2ed01770a6178d1a54b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300a0aba7f88f4587095d097baf365f1",
+    "url": "/React-emoji-search/index.html"
+  },
+  {
+    "revision": "77f607b2dfeb8f9c6b2e",
+    "url": "/React-emoji-search/static/css/main.e694fdac.chunk.css"
+  },
+  {
+    "revision": "da2927d8cd9ba2479ccb",
+    "url": "/React-emoji-search/static/js/2.fa500d2a.chunk.js"
+  },
+  {
+    "revision": "fa9f8d86bca7ba94baa2c0fa4755314d",
+    "url": "/React-emoji-search/static/js/2.fa500d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f607b2dfeb8f9c6b2e",
+    "url": "/React-emoji-search/static/js/main.d0189677.chunk.js"
+  },
+  {
+    "revision": "228c540c1f9237d0a555",
+    "url": "/React-emoji-search/static/js/runtime-main.556bc4f0.js"
+  }
+]);
